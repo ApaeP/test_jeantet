6ecadb164f74225ef9353afddf8afbac09e0e911
Paul Portier <paulportier@gmail.com>
2021-02-02 18:46:33 +0100

    kick sidekiq

--- app/jobs/main_form_job.rb
+++ app/jobs/main_form_job.rb
@@ -1,8 +1,8 @@
 class MainFormJob < ApplicationJob
-  include Sidekiq::Worker
-  queue_as :default
+  # include Sidekiq::Worker
+  # queue_as :default
 
-  def perform(*guests)
-    # Do something later
-  end
+  # def perform(*guests)
+  #   # Do something later
+  # end
 end
