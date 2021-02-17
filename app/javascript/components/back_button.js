592b54afb8d8e5987ccd9df0bf30379123a9582f
Paul Portier <paulportier@gmail.com>
2021-02-17 18:37:05 +0100

    backbtn

--- app/javascript/components/back_button.js
+++ app/javascript/components/back_button.js
@@ -4,6 +4,11 @@ const bindBackButton = () => {
       document.querySelector('#back-link').click();
     })
   };
+  if (document.querySelector('#back-button-results')) {
+    document.querySelector('#back-button-results').addEventListener('click', (e) => {
+      document.querySelector('#back-link').click();
+    })
+  };
 };
 
 export { bindBackButton };
